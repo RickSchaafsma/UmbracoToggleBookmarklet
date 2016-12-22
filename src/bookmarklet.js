59157e0fd5427817ea7e6e0b39b03c6d5b65fe0f
Rick Schaafsma <rick@basicorange.nl>
2016-12-22 14:52:45 +0100

    Copy from https://gist.github.com/apphutdev/d06630ac639e65c8b10757df6b13d0ae src

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+if(document.location.href.indexOf("edit") == -1)
+{
+	document.location.href = '/umbraco#/content/content/edit/' + $("meta[name='node']").attr("content");
+}
+else {
+	document.location.href = document.location.href.replace('/umbraco#/content/content/edit/', "/");
+}
